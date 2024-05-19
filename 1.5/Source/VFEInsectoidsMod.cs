528d1b9797e8c477ec97c0d3f7837ae90c23b0d0
Taranchuk <nfhfyxer@gmail.com>
2024-05-19 18:26:11 +0300

    solution setup

---
+++ 1.5/Source/VFEInsectoidsMod.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace VFEInsectoids
+{
+    public class VFEInsectoidsMod : Mod
+    {
+        public VFEInsectoidsMod(ModContentPack pack) : base(pack)
+        {
+            new Harmony("VFEInsectoidsMod").PatchAll();
+        }
+    }
+}
