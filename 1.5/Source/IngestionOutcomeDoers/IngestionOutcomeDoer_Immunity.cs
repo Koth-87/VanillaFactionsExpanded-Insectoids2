421c0770a39b7762ec4c91ef231a2f1c09e54ef0
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-05-23 13:55:53 +0200

    Moving images around + vines + chitin + jelly

---
+++ 1.5/Source/IngestionOutcomeDoers/IngestionOutcomeDoer_Immunity.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VFEInsectoids
+{
+    internal class IngestionOutcomeDoer_Immunity : IngestionOutcomeDoer
+    {
+        public float percent = 0.05f;
+
+
+
+        public override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            if (pawn.health != null && pawn.health.hediffSet.HasImmunizableNotImmuneHediff() && pawn.health.hediffSet.HasHediff(HediffDefOf.WoundInfection) && pawn.health.immunity != null && pawn.health.immunity.GetImmunity(HediffDefOf.WoundInfection) != 1)
+            {
+                ImmunityRecord cPawn = pawn.health.immunity.GetImmunityRecord(HediffDefOf.WoundInfection);
+                cPawn.immunity += (percent / 100) * ingested.stackCount;
+            }
+        }
+    }
+}
