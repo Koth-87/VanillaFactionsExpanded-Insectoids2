a30a7184ba1602473b2bbdd5ff087b4d34022c05
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-06-19 16:04:16 +0200

    New insect base and custom fire spew ability

---
+++ 1.5/Source/AbilityComps/Properties/CompProperties_AbilityFireSpew.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VFEInsectoids
+{
+    public class CompProperties_AbilityFireSpew : CompProperties_AbilityEffect
+    {
+        public float range;
+
+        public float lineWidthEnd;
+
+        public ThingDef filthDef;
+
+        public float filthChance;
+
+        public int damAmount = -1;
+
+        public EffecterDef effecterDef;
+
+        public bool canHitFilledCells;
+
+        public CompProperties_AbilityFireSpew()
+        {
+            compClass = typeof(CompAbilityFireSpew);
+        }
+    }
+}
