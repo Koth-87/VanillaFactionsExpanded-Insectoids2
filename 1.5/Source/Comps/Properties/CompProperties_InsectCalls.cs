59797b6898e10e71a51ca878b8c0cf639cc63949
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-19 17:39:27 +0200

    Insect call sounds for turrets

---
+++ 1.5/Source/Comps/Properties/CompProperties_InsectCalls.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+using System.Collections.Generic;
+
+namespace VFEInsectoids
+{
+    public class CompProperties_InsectCalls : CompProperties
+    {
+        public IntRange interval;
+        public List<SoundDef> soundDefs;
+
+        public CompProperties_InsectCalls()
+        {
+            this.compClass = typeof(CompInsectCalls);
+        }
+    }
+}
