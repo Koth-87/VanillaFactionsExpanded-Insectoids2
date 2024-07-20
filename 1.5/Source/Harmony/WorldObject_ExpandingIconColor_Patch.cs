fa2d3ab800230968ac9ce9602d384b2b73a1ede3
Taranchuk <nfhfyxer@gmail.com>
2024-07-20 22:59:31 +0300

    emerging hive icon color fix

---
+++ 1.5/Source/Harmony/WorldObject_ExpandingIconColor_Patch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld.Planet;
+using UnityEngine;
+
+namespace VFEInsectoids
+{
+    [HarmonyPatch(typeof(WorldObject), "ExpandingIconColor", MethodType.Getter)]
+    public static class WorldObject_ExpandingIconColor_Patch
+    {
+        public static void Postfix(WorldObject __instance, ref Color __result)
+        {
+            if (__instance is Site site && site.parts != null)
+            {
+                foreach (var part in site.parts)
+                {
+                    if (part.def.Worker is SitePartWorker_InsectHive)
+                    {
+                        __result = __instance.Faction.Color;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
