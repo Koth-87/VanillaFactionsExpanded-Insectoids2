0ecf783cf254b08586f3d4f99fe5757d98738e3c
Taranchuk <nfhfyxer@gmail.com>
2024-07-25 20:01:51 +0300

    prevent colony insects from being dormant

---
+++ 1.5/Source/Harmony/CompCanBeDormant_ToSleep_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEInsectoids
+{
+    [HarmonyPatch(typeof(CompCanBeDormant), "ToSleep")]
+    public static class CompCanBeDormant_ToSleep_Patch
+    {
+        public static bool Prefix(CompCanBeDormant __instance)
+        {
+            if (__instance.parent is Pawn pawn && pawn.IsColonyInsect())
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
