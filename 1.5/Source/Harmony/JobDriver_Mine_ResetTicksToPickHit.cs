d97a31f6ab3042cb4adb1fc208bc699de34454ce
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-08-28 13:51:53 +0200

    Half mining skill for player insectoids

---
+++ 1.5/Source/Harmony/JobDriver_Mine_ResetTicksToPickHit.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace VFEInsectoids
+{
+
+    [HarmonyPatch(typeof(JobDriver_Mine), "ResetTicksToPickHit")]
+    public static class VFEInsectoids_JobDriver_Mine_ResetTicksToPickHit_Patch
+    {
+        public static void Postfix(JobDriver_Mine __instance,ref int ___ticksToPickHit)
+        {
+            if ( __instance.pawn.IsColonyInsect())
+            {
+                ___ticksToPickHit *= 2;
+            }
+        }
+    }
+}
