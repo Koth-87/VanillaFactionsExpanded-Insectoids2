4c541e9cc7ebd9c78feeafdac52e24b8adc8135e
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-08-30 13:57:17 +0200

    Added CompSpawnedMindful that doesn't delete hives with glowpods

---
+++ 1.5/Source/Comps/Properties/CompProperties_MindfulSpawner.cs
@@ -0,0 +1,15 @@
+﻿
+using RimWorld;
+using Verse;
+namespace VFEInsectoids
+{
+    public class CompProperties_MindfulSpawner : CompProperties_Spawner
+    {
+      
+
+        public CompProperties_MindfulSpawner()
+        {
+            compClass = typeof(CompMindfulSpawner);
+        }
+    }
+}
