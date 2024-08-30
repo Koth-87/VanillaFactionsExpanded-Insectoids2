2d2c08a86f926d4fa2f2a340c31be2c2d25583ed
Taranchuk <nfhfyxer@gmail.com>
2024-08-30 18:14:53 +0300

    update

---
+++ 1.5/Source/Harmony/WorkGiver_RemoveBuilding_HasJobOnThing_Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEInsectoids
+{
+    [HarmonyPatch(typeof(WorkGiver_RemoveBuilding), "HasJobOnThing")]
+    public static class WorkGiver_RemoveBuilding_HasJobOnThing_Patch
+    {
+        public static void Postfix(Pawn pawn, Thing t, ref bool __result)
+        {
+            if (__result)
+            {
+                var extension = (t.def.entityDefToBuild ?? t.def).GetModExtension<InsectBuilding>();
+                if (extension is null && pawn.IsColonyInsect())
+                {
+                    __result = false;
+                }
+            }
+        }
+    }
+}
