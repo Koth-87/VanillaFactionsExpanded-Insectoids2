48228de1575c5c9ca50752377f2c2afa014a84cf
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-02-14 16:36:05 +0100

    v3.06

---
+++ 1.6/Source/Harmony/Designator_Deconstruct_CanDesignateThing.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEInsectoids
+{
+    [HarmonyPatch(typeof(Designator_Deconstruct), nameof(Designator_Deconstruct.CanDesignateThing))]
+    public static class VFEInsectoids_Designator_Deconstruct_CanDesignateThing_Patch
+    {
+        public static void Postfix(Thing t, ref AcceptanceReport __result)
+        {
+            if (__result.Accepted && t.def == VFEI_DefOf.VFEI2_InfestedShipChunk)
+            {
+                CompInsectSpawner compInsectSpawner = t.TryGetComp<CompInsectSpawner>();
+                if (compInsectSpawner != null && compInsectSpawner.nextPawnSpawnTick != -1)
+                {
+                    __result = false;
+                }
+            }
+        }
+    }
+}
---
+++ 1.6/Source/Harmony/GenLeaving_GetBuildingResourcesLeaveCalculator.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace VFEInsectoids
+{
+    [HarmonyPatch(typeof(GenLeaving), nameof(GenLeaving.GetBuildingResourcesLeaveCalculator))]
+    public static class VFEInsectoids_GenLeaving_GetBuildingResourcesLeaveCalculator_Patch
+    {
+        private static void Postfix(ref Func<int, int> __result, Thing destroyedThing, DestroyMode mode)
+        {
+            if (destroyedThing?.def != VFEI_DefOf.VFEI2_InfestedShipChunk)
+            {
+                return;
+            }
+            switch (mode)
+            {
+                case DestroyMode.KillFinalize:
+                    __result = (int count) => GenMath.RoundRandom(count);
+                    break;
+                case DestroyMode.Deconstruct:
+                    __result = (int count) => GenMath.RoundRandom(count);
+                    break;
+                case DestroyMode.FailConstruction:
+                    __result = (int count) => GenMath.RoundRandom(count);
+                    break;
+                case DestroyMode.KillFinalizeLeavingsOnly:
+                    break;
+            }
+        }
+    }
+}
